e9e70d3470a79c50d8ed516d16865017b098dd92
baumbart13 <tkefer@tsn.at>
2022-03-28 01:21:05 +0200

    Observer pattern implemented

--- Observer/src/main/java/at/htlinn/Temperatur.java
+++ Observer/src/main/java/at/htlinn/Temperatur.java
@@ -1,4 +1,29 @@
 package at.htlinn;
 
-public class Temperatur implements Observable {
+public class Temperatur extends Observable {
+    private int temperatur;
+
+    public Temperatur(){
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Temperatur";
+    }
+
+    public void setTemperatur(int temperatur) {
+        this.temperatur = temperatur;
+        switch (temperatur) {
+            case 0 -> super.notifyAllMyObservers(0);
+            case 1,2,3,4 -> super.notifyAllMyObservers(1);
+            case 5,6,7 -> super.notifyAllMyObservers(2);
+            case 8,9,10 -> super.notifyAllMyObservers(3);
+            default -> super.notifyAllMyObservers(-1);
+        }
+    }
+
+    public int getTemperatur() {
+        return temperatur;
+    }
 }
